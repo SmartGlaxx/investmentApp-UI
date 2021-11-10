2c26c402a4d2a6f75889e3d65be918aa66e6a88a
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-11-10 11:48:49 +0100

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import {HeroButton, DashboardButton, DashboardButtonSecond, AuthButton, LoginSignupBtn} from "./buttons"
+import Header from "./header"
+import Footer from "./footer"
+import DashboardHeader from "./dashboardHeader"
+import Hero from './hero'
+import {InvestorSidebarMobile} from './investorSidebar'
+import {InvestorSidebar} from './investorSidebar'
+import { SidebarMobile } from "./sidebar"
+import {Sidebar} from './sidebar'
+import ProfilePopover from "./profilePopover"
+
+import Packages from './packages'
+
+export {Hero, HeroButton, DashboardButton, DashboardButtonSecond, AuthButton, LoginSignupBtn, Header, Footer,  DashboardHeader, InvestorSidebarMobile, 
+    InvestorSidebar, Sidebar, SidebarMobile, Packages, ProfilePopover}
